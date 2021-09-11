09bd1c83a6566b0ecb83411faeb6e64e8717d9bd
MiikeVI <miguelandresv14@gmail.com>
2021-09-11 17:19:10 -0300

    - fix: optional variables

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT, process.env.SERVER);
+  await app.listen(process.env.PORT || 3000, process.env.SERVER || '0.0.0.0');
 }
 bootstrap();
