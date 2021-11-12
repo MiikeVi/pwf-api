df0d62bd76a963bae3950b383a4ec4c2fff83bc1
MiikeVI <miguelandresv14@gmail.com>
2021-11-11 22:53:40 -0300

    + fix revert cors changes

--- src/main.ts
+++ src/main.ts
@@ -5,35 +5,11 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const cors = require('cors');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use( 
-    cors({
-      allowedHeaders: ['sessionId', 'Content-Type'],
-      exposedHeaders: ['sessionId'],
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-    }),
-  );
-
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-  });
+  app.enableCors();
 
   app.setGlobalPrefix('api');
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-  });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(
     bodyParser.urlencoded({
